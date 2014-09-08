9e8c9f5204c736c4334b93daedf5dc6f704fd753
1210la <1210la@LOPEZMONTENEGRO>
2014-09-07 23:40:23 -0600

    new commit

---
+++ HojaDeTrabajo6.java
@@ -0,0 +1,22 @@
+/*
+ *  Universidad del Valle de Guatemala
+ *  Algoritmos y estructura de datos 2014
+ *  
+ *  Autores:    Nancy Girón Muñoz - 13467
+ *              Martín Meyer Ramazzini - 13043
+ *              Alberto López Montenegro - 13181
+ */
+public class HojaDeTrabajo6 {
+	//public void catchPerson(Persona in){}
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+		Factory F=new Factory();
+		FrameIngreso FI=new FrameIngreso();
+		FI.run();
+		//FI.run(this);
+    }
+    
+}
